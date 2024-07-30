09a89b0f1a8e54985129c200ee3047429360051f
ozkan <ozkandnz09@gmail.com>
2024-07-30 22:24:37 +0300

    a

---
+++ app.js
@@ -0,0 +1,7 @@
+document.querySelector(".header").style.backgroundColor = "#ff4500";
+document.querySelector(".header").style.color  = "white";
+document.querySelector(".header").style.padding = "2rem";
+document.querySelector(".header").style.textAlign = "center";
+document.getElementById("title").textContent = "Javascript Dom Assignment";
+document.querySelector(".nav-item").style.listStyle = "none";
+document.querySelector(".nav-item").style.flex = "center";
